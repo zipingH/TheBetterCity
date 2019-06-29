fe0fda0f3cc9b2db3a908a8ca5f883a13928ef1b
Maithri-CH <maithrich3@gmail.com>
2019-06-28 22:42:16 -0700

    updating username,password

--- application/team4-development/config/db.conf.js
+++ application/team4-development/config/db.conf.js
@@ -6,10 +6,10 @@ const mysql = require('mysql')
 
 const db = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '@Test123',
-    database: 'library3',
+    host: 'ec2-18-216-80-193.us-east-2.compute.amazonaws.com',
+    user: 'ubuntu',
+    password: 'csc648',
+    database: '648_db',
     multipleStatements: true
 })
 
