99e07b4b313e0137b69853921387f5b2b898fcc5
Ziping <tomhuang1997@gmail.com>
2019-07-16 15:12:47 -0700

    created routes to search all issue and percentlike

--- application/team4-development/routes/getIssue.js
+++ application/team4-development/routes/getIssue.js
@@ -19,8 +19,12 @@ Object.prototype.isEmpty = function() {
     }
     return true;
 }
+
+//route for getting a category by id
 router.get('/getCategory', (req, res) =>{
-const queryString = "SELECT issue.title, issue.photo, issue.location, issue.description, issue.time_stamp FROM issue , category where issue.category_id = ? AND category.category_id = ?;"
+const queryString = "SELECT issue.id, issue.title, issue.photo, issue.location, issue.description, issue.time_stamp \
+FROM issue , category\
+WHERE issue.category_id = ? AND category.category_id = ?;"
 const category_id = req.query.category_id
 db.query(queryString, [category_id, category_id], (err, results)=>{
     if (err) {
@@ -34,10 +38,37 @@ db.query(queryString, [category_id, category_id], (err, results)=>{
 })
 })
 
+//route for getting all issue
+router.get('/getAllCategory', (req, res) =>{
+    const queryString = "SELECT issue.id, issue.title, issue.photo, issue.location, issue.description, issue.time_stamp\
+    FROM issue\
+    where ?=?;"
+    var whereAll = 1;
+    db.query(queryString,[whereAll,whereAll], (err, results)=>{
+        if (err) {
+            return res.status(400).send({
+                err
+            });
+        } else {
+            res.send(results)
+        }
+    })
+    })
+
+//route using percent like to search issue where park is in the title
 router.get('/getPercentLike', (req, res) =>{
-
+    const queryString = "SELECT issue.id, issue.title, issue.photo, issue.location, issue.description, issue.time_stamp \
+    FROM issue\
+    where title like '%park%';"
+    db.query(queryString,(err, results)=>{
+        if(err) {
+            throw err;
+            console.log(results);
+        }
+        else{
+            res.send(results);
+        }
+    })
 })
-
-
 
 module.exports = router
