576824b08c08f656e5b2aaa82c2c5091f8397383
Maithri-CH <maithrich3@gmail.com>
2019-07-30 21:36:34 -0700

    Adding session storage to search keyword

--- application/team4-development/views/js/sessionStorage.js
+++ application/team4-development/views/js/sessionStorage.js
@@ -3,14 +3,19 @@ $(function () {
         $("#searchBtn").click(function () {
 
              var filterSel =  $("#filter option:selected").text();
-
               sessionStorage.setItem("sele", filterSel);
+
+              var searchSel =   $("#search").val();
+              sessionStorage.setItem("sele2", searchSel);
+
               });
 
               var ty = sessionStorage.getItem("sele");
+                var ty2 = sessionStorage.getItem("sele2");
 
               if(ty){
                 //Set the value of select from sessionStorage
                     $('#filter option').filter(function () { return $(this).html() == ty; }).prop('selected', true);
+                    $("#search").attr({"placeholder" : ty2});
                }
             });
