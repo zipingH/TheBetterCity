c2a20e0905c86df1dca63b19c7443804714f3cf4
Ziping <tomhuang1997@gmail.com>
2019-08-10 19:04:42 -0700

    Fixed postIssue.js route error when no photo is added

--- application/team4-development/routes/postIssue.js
+++ application/team4-development/routes/postIssue.js
@@ -47,7 +47,14 @@ const router = express.Router()
 router.post('/postIssue', upload.single('photo'), (req, res) => {
     var title = req.body.title;
     //const photoPath = "../IssueImages/";
-    var photo = req.file.filename;
+    var photo;
+    if(photo == null){
+        photo = '';
+        console.log('No Photo added');
+    }else{
+        photo = req.file.filename;
+        console.log('Photo has added with filename');
+    }
     var location = req.body.street + ", " + req.body.city + ", " + req.body.state + ", " + req.body.zip;
     var description = req.body.description;
     var category = req.body.category;
@@ -58,12 +65,13 @@ router.post('/postIssue', upload.single('photo'), (req, res) => {
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date + ' ' + time;
     const query_postIssue = "INSERT INTO issue(issue_id, title, photo, location, description, status_id, user_id, time_stamp, category_id) VALUES (@issue_id, ? , concat('../IssueImages/', ? ), ? , ? , '6',(SELECT id FROM user where email = ? ), ? , (SELECT category_id FROM category WHERE category = ? ));"
-
+    
     db.query(query_postIssue, [title, photo, location, description, email, dateTime, category], (err, result) => {
         if (!req.file) {
-            console.log("No file received");
+            message = "No photo added"
+            console.log("No photo received");
             return res.send({
-                success: false
+                message: message
             });
         }
 
@@ -79,7 +87,6 @@ router.post('/postIssue', upload.single('photo'), (req, res) => {
             console.log('Body: ', req.body);
             return res.status(200).send({
                 message: message,
-                success: true
             });
 
         }
