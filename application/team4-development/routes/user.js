c4e1b902b11ce4c50902a4927f84db20e39594e7
pyropat <patreckraymore@gmail.com>
2019-08-11 18:08:38 -0700

    added the password encryption using bcrypt

--- application/team4-development/routes/user.js
+++ application/team4-development/routes/user.js
@@ -12,6 +12,9 @@ const db = require('../config/db.conf.js');
 var path = require('path');
 var fs = require('fs');
 var session = '';
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const { check, validationResult } = require('express-validator');
 
 //create router
 const router = express.Router()
@@ -36,25 +39,29 @@ router.post('/login' , (req, res) =>{
                 console.log('Email not found in DB');
                 res.render('home');
             }
-            else if (email == userDetail.email && password == userDetail.password) {
-                req.session.user = userDetail;
-                //res.status(200).json(userDetail).end();
-                res.render('home',{session: req.session});
-                  console.log("--login successful  --");
-                  console.log('session:' + req.session);
-                  console.log('  req.session.user :' + req.session.user);
+            //compares hashed password
+            else if (email == userDetail.email){
+              bcrypt.compare(password, userDetail.password, function(err, result) {
+                if (result == true) {
+                  req.session.user = userDetail;
+                  res.render('home',{session: req.session});
+                    console.log("--login successful  --");
+                    console.log('session:' + req.session);
+                    console.log('  req.session.user :' + req.session.user);
                     console.log('  session.user.id :' + req.session.user.id);
-                      console.log('  session.user.name :' + req.session.user.name);
-                        console.log('  session.user.email :' + req.session.user.email);
-            }
-            else {
-              console.log('Password doesnt match ');
-                res.redirect('home');
-            }
+                    console.log('  session.user.name :' + req.session.user.name);
+                    console.log('  session.user.email :' + req.session.user.email);
+                } else {
+                  console.log('Password doesnt match ');
+                  console.log(userDetail.password);
+                  console.log(password);
+                  res.redirect('home');
+                }
+              });
+          }
         }
-    })
-})
-
+      });
+});
 
 //route for login
 router.post('/reset' , (req, res) =>{
@@ -64,22 +71,27 @@ router.post('/reset' , (req, res) =>{
   //route for signup/register
   router.post('/register' , (req, res) =>{
 
+
     console.log("inside register");
     console.log('name:' + req.body.name);
     console.log('email:' +req.body.email);
+    //hash password
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      const queryString = "INSERT INTO user (name, email, password, role) VALUES (?, ?, ?, 'user')";
+      console.log('password:' +hash);
+      db.query(queryString, [req.body.name, req.body.email, hash], (err, results) => {
+        if (err) {
+            return res.status(400).send({
+                err
+            });
+        } else {
+            res.render('Home');
+        }
+    });
+  });
 
-    const queryString = "INSERT INTO user (name, email, password, role) VALUES (?, ?, ?, 'user')";
 
-    db.query(queryString, [req.body.name, req.body.email, req.body.password], (err, results) => {
-      if (err) {
-          return res.status(400).send({
-              err
-          });
-      } else {
-          res.render('Home');
-      }
-  })
-})
+});
 
   //route for logout
   router.get('/logout' , (req, res) =>{
