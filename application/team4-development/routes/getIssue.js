bb8133b3ec64b9d114b3af990b353595d099ebe3
Maithri-CH <maithrich3@gmail.com>
2019-08-13 12:01:40 -0700

    Reverting Getissues to an older version,moving the no result found section after the title in Home page

--- application/team4-development/routes/getIssue.js
+++ application/team4-development/routes/getIssue.js
@@ -20,184 +20,105 @@ Object.prototype.isEmpty = function() {
   return true;
 }
 
-router.post('/search', (req, res) => {
-      const searchBy = req.body.searchBy;
-      const searchText = req.body.searchText;
-      const filters = req.body.filters;
-      var queryParams = [];
+//route for getting a filter and search
+router.get('/dropdown', (req, res) => {
 
-      var query = 'select * from issue';
+  const category_id = req.query.category_id;
+  const title = req.query.title;
+  const status_id = req.query.status_id;
+  var pageNo = req.query.pageNo;
+  console.log(req.query)
+  console.log("inside filter dropdown:--");
+  console.log('filter:' + category_id + ' status_id: ' + status_id);
+  console.log('search:' + title);
+  console.log('Page No :' + pageNo);
+  var pageNo = req.query.pageNo ? req.query.pageNo : 0;
+  var pageSize = 4;
+  var startIndex = 0;
+  var endIndex = 3;
 
-      if (searchBy == 'location') {
-        query = query + 'where location LIKE concat(' % ', ? ,' % ')';
-        queryParams[0] = searchText;
-      } else if (searchBy == 'title') {
-        query = query + 'where title LIKE concat(' % ', ? ,' % ')';
-        queryParams[0] = searchText;
-      }
-
-      if (filters.categories) {
-        query = query + ' and category_id IN ?';
-        queryParams[1] = filters.categories;
-      }
-      if (filters.status) {
-        query = query + ' and status IN ?';
-        queryParams[1] = filters.status;
-      }
-      db.query(query, queryParams, (req, res) => {
-        if (err) {
-          return res.status(400).send({
-            err
-          });
-        } else {
-          var length = results.length;
-          var displayed = pageNo * pageSize;
-          if (pageSize < length) {
-            results = results.splice(displayed, pageSize);
-          }
-          var displaying = displayed + pageSize >= length ? length : (displayed + pageSize);
-
-          res.render('Home', {
-            'parks': results,
-            'pageSize': pageSize,
-            'pageNo': pageNo,
-            'from': displayed + 1,
-            'to': displaying,
-            'total': length
-          });
+  const query_percentLikeByTitle = "SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
+  //const queryFilter = "SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
+  //const querySearch = "SELECT * FROM issue, status, category WHERE issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
+  //const queryString = "SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
+  //const queryString2= "SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.status_id = status.status_id AND issue.category_id = category.category_id INTERSECT SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
+  const query_selectAll = "SELECT * FROM issue, status, category WHERE issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
+  const query_selectStatus = "SELECT * FROM issue, status, category WHERE  issue.status_id = ? AND issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
+  if (category_id == 0) {
+    db.query(query_selectAll, [title], (err, results) => {
+      if (err) {
+        return res.status(400).send({
+          err
+        });
+      } else {
+        var length = results.length;
+        var displayed = pageNo * pageSize;
+        if (pageSize < length) {
+          results = results.splice(displayed, pageSize);
         }
-      });
-});
-      //route for getting a filter and search
-      router.get('/dropdown', (req, res) => {
+        var displaying = displayed + pageSize >= length ? length : (displayed + pageSize);
 
-            const category_id = req.query.category_id
-            const title = req.query.title
-            const status_id = req.query.status_id
-            const location = req.query.location
-            console.log(req.query)
-            console.log("inside filter dropdown:--");
-            console.log('filter:' + req.query.category_id + ' ' + status_id);
-            console.log('search:' + req.query.title);
-            //const percent = req.query.title
-            //    console.log('title'+ @title);
+        res.render('Home', {
+          'parks': results,
+          'pageSize': pageSize,
+          'pageNo': pageNo,
+          'from': displayed + 1,
+          'to': displaying,
+          'total': length
+        });
+      }
+    })
+  } else if (category_id > 0 || category_id < 6) {
+    db.query(query_percentLikeByTitle, [category_id, title], (err, results) => {
+      if (err) {
+        return res.status(400).send({
+          err
+        });
+      } else {
+        var length = results.length;
+        var displayed = pageNo * pageSize;
+        if (pageSize < length) {
+          results.splice(displayed, pageSize);
+        }
+        var displaying = displayed + pageSize >= length ? length : (displayed + pageSize);
+        res.render('Home', {
+          'parks': results,
+          'pageSize': pageSize,
+          'pageNo': pageNo,
+          'from': displayed + 1,
+          'to': displaying,
+          'total': length
+        });
+      }
+    })
+  }
 
-            const query_percentLikeByTitle = "SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
-            //const queryFilter = "SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
-            //const querySearch = "SELECT * FROM issue, status, category WHERE issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
-            //const queryString = "SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
-            //const queryString2= "SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.status_id = status.status_id AND issue.category_id = category.category_id INTERSECT SELECT * FROM issue, status, category WHERE issue.category_id = ? AND issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
-            const query_selectAll = "SELECT * FROM issue, status, category WHERE issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
-            const query_selectStatus = "SELECT * FROM issue, status, category WHERE  issue.status_id = ? AND issue.title LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id;"
-            const query_location = "SELECT * FROM issue, status, category WHERE issue.location LIKE concat('%', ? ,'%') AND issue.status_id = status.status_id AND issue.category_id = category.category_id"
-
-            if (category_id == 0) {
-              db.query(query_selectAll, [title], (err, results) => {
-                if (err) {
-                  return res.status(400).send({
-                    err
-                  });
-                } else {
-                  var length = results.length;
-                  var displayed = pageNo * pageSize;
-                  if (pageSize < length) {
-                    results = results.splice(displayed, pageSize);
-                  }
-                  var displaying = displayed + pageSize >= length ? length : (displayed + pageSize);
-
-                  res.render('Home', {
-                    'parks': results,
-                    'pageSize': pageSize,
-                    'pageNo': pageNo,
-                    'from': displayed + 1,
-                    'to': displaying,
-                    'total': length
-                  });
-                }
-              })
-            }
-
-               if (category_id > 0 || category_id < 6) {
-                db.query(query_percentLikeByTitle, [category_id, title], (err, results) => {
-                  if (err) {
-                    return res.status(400).send({
-                      err
-                    });
-                  } else {
-                    var length = results.length;
-                    var displayed = pageNo * pageSize;
-                    if (pageSize < length) {
-                      results = results.splice(displayed, pageSize);
-                    }
-                    var displaying = displayed + pageSize >= length ? length : (displayed + pageSize);
-
-                    res.render('Home', {
-                      'parks': results,
-                      'pageSize': pageSize,
-                      'pageNo': pageNo,
-                      'from': displayed + 1,
-                      'to': displaying,
-                      'total': length
-                    });
-                  }
-                })
-              }
-
-                // dropdown menu route to select status if category_id is not in the database
-                 if (status_id > 5) {
-                  db.query(query_selectStatus, [status_id, title], (err, results) => {
-                    if (err) {
-                      return res.status(400).send({
-                        err
-                      });
-                    } else {
-                      var length = results.length;
-                      var displayed = pageNo * pageSize;
-                      if (pageSize < length) {
-                        results = results.splice(displayed, pageSize);
-                      }
-                      var displaying = displayed + pageSize >= length ? length : (displayed + pageSize);
-
-                      res.render('Home', {
-                        'parks': results,
-                        'pageSize': pageSize,
-                        'pageNo': pageNo,
-                        'from': displayed + 1,
-                        'to': displaying,
-                        'total': length
-                      });
-                    }
-                  });
-                }
-
-                  //dropdown menu to search by location
-                  else {
-                    db.query(query_location, [location], (err, results) => {
-                      if (err) {
-                        return res.status(400).send({
-                          err
-                        });
-                      } else {
-                        var length = results.length;
-                        var displayed = pageNo * pageSize;
-                        if (pageSize < length) {
-                          results = results.splice(displayed, pageSize);
-                        }
-                        var displaying = displayed + pageSize >= length ? length : (displayed + pageSize);
-
-                        res.render('Home', {
-                          'parks': results,
-                          'pageSize': pageSize,
-                          'pageNo': pageNo,
-                          'from': displayed + 1,
-                          'to': displaying,
-                          'total': length
-                        });
-                      }
-                    })
-                  }
-                });
+  // dropdown menu route to select status if category_id is not in the database
+  else {
+    db.query(query_selectStatus, [status_id, title], (err, results) => {
+      if (err) {
+        return res.status(400).send({
+          err
+        });
+      } else {
+        var length = results.length;
+        var displayed = pageNo * pageSize;
+        if (pageSize < length) {
+          results.splice(displayed, pageSize);
+        }
+        var displaying = displayed + pageSize >= length ? length : (displayed + pageSize);
+        res.render('Home', {
+          'parks': results,
+          'pageSize': pageSize,
+          'pageNo': pageNo,
+          'from': displayed + 1,
+          'to': displaying,
+          'total': length
+        });
+      }
+    })
+  }
+})
 
 
-
-                    module.exports = router
+module.exports = router
